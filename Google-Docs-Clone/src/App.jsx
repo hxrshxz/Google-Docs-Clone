63c5add9430089d463517ae96167806859ddb35d
Harsh <harshmastic@gmail.com>
2024-11-02 16:11:06 +0530

    Quill Setup And implemented

---
+++ Google-Docs-Clone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TextEditor from './Components/TextEditor'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <TextEditor/>
+    </div>
+  )
+}
--- Google-Docs-Clone/src/Components/TextEditor.jsx
+++ Google-Docs-Clone/src/Components/TextEditor.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { useCallback, useRef } from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['bold', 'italic', 'underline'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ align: [] }],
+    ['image', 'blockquote', 'code-block'],
+    ['clean'],
+]
 
 export default function TextEditor() {
-  return (
-    <div>
-        Text Editor
-    </div>
-  )
+
+
+
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return;
+        wrapper.innerHTML = '';  // Clear the container
+
+        const editor = document.createElement('div');
+        wrapper.append(editor);
+        new Quill(editor, {
+            theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS },
+            
+        });
+
+
+    }, []);
+
+    return (
+        <div className='container' ref={wrapperRef}>
+            more text
+        </div>
+    );
+
 }
